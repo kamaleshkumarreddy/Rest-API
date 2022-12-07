91281e9a57f8d2ac8f937fe9ab7a35e88d1add01
kamaleshkumarreddy <108462892+kamaleshkumarreddy@users.noreply.github.com>
2022-12-07 11:36:35 +0530

    Add files via upload
    Initial commit

---
+++ SampleTest.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+    @Test
+    public void shouldTestSomething(){
+        Assert.assertEquals(1,1);
+    }
+
+}
